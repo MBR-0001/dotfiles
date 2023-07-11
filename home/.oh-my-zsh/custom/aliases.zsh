10110505315ec463b40d7be16de22b50ee38307c
MBR-0001 <55142207+MBR-0001@users.noreply.github.com>
2023-07-11 19:10:03 +0200

    Update aliases.zsh

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,6 @@
 alias commit='f() { git add . && git commit -m "$1" && git push };f'
 alias temp='echo "$(($(cat /sys/class/thermal/thermal_zone*/temp) / 1000))°C"'
 alias prismrdp='rdesktop -u Rust -p - rust.kngsgaming.network -g 1920x1080'
-alias sus='echo amogus'
 alias tempmonitor='while true; do; temp; sleep 1; done'
-alias gpumonitor='while true; do; nvidia-smi; sleep 1; done'
+alias gpumonitor='while true; do; sensors | grep 'junction:' && sensors | grep 'fan1:'; sleep 1; done'
 alias ~='cd ~'
