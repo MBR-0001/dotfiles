f41cb915c6be690f62fd68f7a80fddc3430fce2c
MBR-0001 <55142207+MBR-0001@users.noreply.github.com>
2024-06-01 00:04:25 +0200

    Update aliases.zsh

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,5 @@
 alias commit='f() { git add . && git commit -m "$1" && git push };f'
-alias temp='echo "$(($(cat /sys/class/thermal/thermal_zone*/temp) / 1000))°C"'
-alias prismrdp='rdesktop -u Rust -p - rust.kngsgaming.network -g 1920x1080'
+alias temp='echo "$(($(cat /sys/devices/pci0000:00/*/hwmon/hwmon1/temp1_input) / 1000))°C"'
 alias tempmonitor='while true; do; temp; sleep 1; done'
 alias gpumonitor='while true; do; sensors | grep 'junction:' && sensors | grep 'fan1:'; sleep 1; done'
 alias ~='cd ~'
